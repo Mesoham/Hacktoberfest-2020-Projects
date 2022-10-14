b482e6d6a9a6f1093be492b13c63d4c68d6c1cc3
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-14 20:25:47 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,5 @@
-print("Calculator")
-num1=float(input())
-operator=input()
-num2=float(input())
+# Program make a simple calculator
 
-if operator == "+":
-  print(num1+num2)
-elif operator == "*":
-  print(num1*num2)
-elif operator == "/":
-  print(num1/num2)
-elif operator == "-":
-  print(num1-num2)
-else:
-  print("Operater didn't found")
-  =======
 # This function adds two numbers
 def add(x, y):
     return x + y
@@ -31,17 +17,17 @@ def divide(x, y):
     return x / y
 
 
-print("Select operation: ")
+print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
 while True:
-    # Take input from the user
+    # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
-    # Check if choice is one of the four options
+    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
@@ -57,33 +43,12 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-        break
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
     else:
         print("Invalid Input")
-
-        
-        #basic calculator
-print("select operation")
-print("1.add")
-print("2.subtract")
-print("3.multiply")
-print("4.division")
-choice=input("enter choice 1/2/3/4/: ")
-number1=int(input("enter number 1: "))
-number2=int(input("enter number 2: "))
-if choice == "1":
-    
-   print(number1 + number2)
-elif choice == "2":
-    
-   print(number1 - number2)
-elif choice =="3":
-    
-   print(number1 * number2)
-elif choice == "4":
-    
-   print(number1 / number2)
-else:
-    print("invalid")
-
-        
